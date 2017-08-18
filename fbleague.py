69d2b3c5c4f24d58a0bd1af76247888a3fa5b742
Testdrivenapproach <chukwuemekambaebie@gmail.com>
2017-08-18 19:01:05 +0100

    Fri, Aug 18, 2017  7:01:05 PM

--- fbleague.py
+++ fbleague.py
@@ -1,25 +1,59 @@
+class FootballLeaguePerformance(object):
 
-filename = 'football-league-results.txt'
+    filename = 'football-league-results.txt'
 
-def fbleagueResult():
-    with open(filename) as fbresult:
-        fbresult.next()
-        for team in fbresult:
-            team = team.split()
-            clubNames = []
-            clubData = []
+    def fbleagueResult(filename):
+        with open(filename) as fbresult:
+            clubNamesList =[]
+            goalDiff = []
+            # smallestClubWithGoalDiff = []
+            # smallestGoalDiff = []
+            fbresult.next()
+            newResult = []
+            # tempGoalFor = []
+            # tempGoalAgainst = []
+            for team in fbresult:
+                team = team.split()
+                clubNames = []
+                clubData = []
 
-            for data in team:
-                try:
-                    data = int(data)
-                    clubData.append(data)
+                for data in team:
 
-                except ValueError:
-                    stringData = str(data)
-                    if stringData != "-":
-                        clubNames.append(stringData)
-                    pass
+                    try:
+                        data = int(data)
+                        clubData.append(data)
+                        pass
+                    except ValueError:
+                        stringData = str(data)
+                        if stringData != "-":
+                            clubNames.append(stringData)
+                        pass
+                if len (clubData) > 0:
+                    goalFor = int(clubData[4])
+                    goalAgainst = int(clubData[5])
+                    del clubNames[:1]
+                    clubNames = ''.join(clubNames)
+                    clubNamesList.append(clubNames)
 
-            print clubNames
-            print clubData
-fbleagueResult()
+                    goalDiff.append(goalFor - goalAgainst)
+                    # goalDiff.append(diffGoal)
+
+                #print teamGoalAgainst
+                #print clubData
+                #print clubNames, clubGoalDiff
+            clubGoalPerformance = dict(zip(clubNamesList, goalDiff))
+                #print clubGoalPerformance
+            teamWithLowestGoalDiff = min(sorted(clubGoalPerformance.keys()))
+            lowestGoalDiff = min(sorted(clubGoalPerformance.values()))
+
+            teamWithLowestGoalDiff = str(teamWithLowestGoalDiff)
+            lowestGoalDiff = str(lowestGoalDiff)
+
+                # smallestClubWithGoalDiff.append(lowestclubGoalDiff)
+
+            print "The Club with the lowest goal difference is " + teamWithLowestGoalDiff + ", with: " + lowestGoalDiff + " goal difference"
+
+                #print goalFor, goalAgainst
+                #print teamGoalPerformance
+
+    fbleagueResult(filename)
